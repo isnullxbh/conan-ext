f792a7794bbb51eb1a24a72c4985fd27f519df0a
isnullxbh <isnullxbh@gmail.com>
2021-06-23 18:10:44 +0700

    Closes #1: add support to switch compilation mode (header-only/compiled)

--- all/conanfile.py
+++ all/conanfile.py
@@ -1,6 +1,6 @@
 import os
 
-from conans import ConanFile, tools
+from conans import ConanFile, CMake, tools
 
 
 class ExtConan(ConanFile):
@@ -10,20 +10,46 @@ class ExtConan(ConanFile):
     url = "https://github.com/isnullxbh/conan-ext"
     description = "The C++ standard library extensions"
     topics = ("cpp", "cpp20", "cpp-library")
-    # settings = "os", "compiler", "build_type", "arch"
-    # options = {"shared": [True, False], "fPIC": [True, False]}
-    # default_options = {"shared": False, "fPIC": True}
-    # generators = "cmake"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False], "header_only": [True, False]}
+    default_options = {"shared": False, "fPIC": True, "header_only": True}
+    generators = "cmake"
 
     _source_subfolder = "source_subfolder"
 
+    def configure(self):
+        if self.options.header_only:
+            self.settings.clear()
+            del self.options.shared
+            del self.options.fPIC
+
     def package_id(self):
-        self.info.header_only()
+        if self.options.header_only:
+            self.info.header_only()
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
 
+    def build(self):
+        if not self.options.header_only:
+            cmake = CMake(self)
+            cmake.configure(source_folder=self._source_subfolder)
+            cmake.build()
+
     def package(self):
-        include_dir = os.path.join(self._source_subfolder, "include")
+        inc_dir = os.path.join(self._source_subfolder, "include")
+        src_dir = os.path.join(self._source_subfolder, "src")
+
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        self.copy(pattern="*.hpp", dst="include", src=include_dir)
+        self.copy(pattern="*.hpp", dst="include", src=inc_dir)
+        self.copy(pattern="*.cpp", dst="src", src=src_dir)
+        self.copy(pattern="*.lib", dst="lib", keep_path=False)
+        self.copy(pattern="*.so", dst="lib", keep_path=False)
+        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+        self.copy(pattern="*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        if self.options.header_only:
+            self.cpp_info.defines.append("HEADER_ONLY")
+        else:
+            self.cpp_info.libs = ["ext"]
